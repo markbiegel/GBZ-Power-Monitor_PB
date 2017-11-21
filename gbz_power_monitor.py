cb2826533279dbdd48715eafb1c5138c372741a3
Mark Biegel <markbiegel@gmail.com>
2017-11-21 16:05:01 +1000

    pins

--- gbz_power_monitor.py
+++ gbz_power_monitor.py
@@ -14,7 +14,7 @@ import time
 batteryGPIO    = 17  # GPIO 17/pin 0
 powerGPIO      = 27  # GPIO 27/pin 2
 redLEDGPIO     = 21   # GPIO 23 /pin 16
-greenLEDGPIO   = 4   # GPIO 24 /pin 18
+greenLEDGPIO   = 3   # GPIO 24 /pin 18
 sampleRate     = 0.1 # tenth of a second
 batteryTimeout = 10  # 10 seconds
 powerTimeout   = 1   # 1 second
